44fe86f4be4227c788e54c61913f25fe5ca62ade
artuc2025 <artuc2025@gmail.com>
2025-04-13 17:40:12 +0400

    delete all files

--- app.config.ts
+++
@@ -1,6 +0,0 @@
-// app.config.ts
-export default defineAppConfig({
-    name: 'Nuxt Blog App',
-    description: 'A Nuxt project to learn everything step-by-step',
-  })
-  
--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="text-center mt-10">
-      <h1 class="text-4xl font-bold">Welcome to My Nuxt Blog</h1>
-      <p class="mt-2 text-gray-500">This is the homepage</p>
-    </div>
-  </template>
